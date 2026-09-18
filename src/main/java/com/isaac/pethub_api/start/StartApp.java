7c2376802fa63de263f093542c2242bdcc76b5ef
Isaac Medeiros <joseisaac.isaac123@gmail.com>
2026-09-18 15:20:31 -0300

    fix: nome da variavel do banco no env.example

--- src/main/java/com/isaac/pethub_api/start/StartApp.java
+++ src/main/java/com/isaac/pethub_api/start/StartApp.java
@@ -1,4 +1,13 @@
 package com.isaac.pethub_api.start;
 
-public class StartApp {
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartApp implements ApplicationRunner {
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Testando metodo que sera executado pelo spring");
+    }
 }
